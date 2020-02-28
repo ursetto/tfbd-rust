239d1052d1ea0b77f5a9cccff51d19d70d487653
Jim Ursetto <jim@3e8.org>
2020-02-28 16:12:11 -0600

    read 2x section count

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
 use std::io;
+use std::io::prelude::*; // Read
 use std::fs::File;
 use byteorder::{LE, ReadBytesExt};
 
@@ -16,10 +17,20 @@ fn main() {
     }
 }
 
+// TODO: decode should take a writer and have decode_file do the
+// file open, for easier testing and composability.
 fn decode(filename: PathBuf) -> io::Result<()> {
     let mut f = File::open(&filename)?;
     println!("decode filename: {}", filename.display());
     let record_count = f.read_u16::<LE>()?;
     println!("# TFBD ({} records total)", record_count);
+    decode_2x(f)?;
+    Ok(())
+}
+
+// TODO: Should take a Read trait.
+fn decode_2x(mut f: std::fs::File) -> io::Result<()> {
+    let section_count = f.read_u16::<LE>()?;
+    println!("# 2x section ({} records)", section_count);
     Ok(())
 }
