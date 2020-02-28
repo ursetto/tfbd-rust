94f9125fa343fdfd05e33c74f27f05adee8dfad6
Jim Ursetto <jim@3e8.org>
2020-02-28 15:34:32 -0600

    read record count from TFBD file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
+use std::io;
+use std::fs::File;
+use byteorder::{LE, ReadBytesExt};
 
 #[derive(StructOpt)]
 enum Cli {
@@ -9,10 +12,14 @@ enum Cli {
 fn main() {
     let args = Cli::from_args();
     match args {
-        Cli::Decode { filename } => { decode(filename); }
+        Cli::Decode { filename } => { decode(filename).unwrap(); }
     }
 }
 
-fn decode(filename: PathBuf) {
+fn decode(filename: PathBuf) -> io::Result<()> {
+    let mut f = File::open(&filename)?;
     println!("decode filename: {}", filename.display());
+    let record_count = f.read_u16::<LE>()?;
+    println!("# TFBD ({} records total)", record_count);
+    Ok(())
 }
