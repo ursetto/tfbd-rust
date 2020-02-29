d456bc830e1f17e034033b430301a92d5209aceb
Jim Ursetto <jim@3e8.org>
2020-02-28 18:40:51 -0600

    use match instead of if/else for rtype

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
 fn decode_file(filename: PathBuf) -> io::Result<()> {
     let f = File::open(&filename)?;
     let r = io::BufReader::new(f);
-    println!("Decoding filename: {}", filename.display());
+    // println!("Decoding filename: {}", filename.display());
     decode(r)?;
     Ok(())
 }
@@ -41,27 +41,15 @@ fn decode_2x(mut r: impl io::Read) -> io::Result<()> {
         let area_len = r.read_u16::<LE>()?;
         assert!(rtype & 0xf0 == 0x20, "expected 2x (0x20) section");
         assert_eq!(var_len, 0, "2x section var_len must be 0");
-        if rtype == 0x20 {
-            println!("DB  +${:04X}, ${:02X}", offset, area_len);
-        }
-        else if rtype == 0x21 {
-            println!("DW  +${:04X}, ${:02X}", offset, area_len);
-        }
-        else if rtype == 0x23 {
-            println!("DA  +${:04X}, ${:02X}", offset, area_len);
-        }
-        else if rtype == 0x27 {
-            println!("HEX +${:04X}, ${:02X}", offset, area_len);
-        }
-        else if rtype == 0x28 {
-            println!("DS  +${:04X}, ${:02X}", offset, area_len);
-        }
-        else if rtype == 0x29 {
-            println!("ASC +${:04X}, ${:02X}", offset, area_len);
-        }
-        else {
-            println!("rtype {:02X} var_len {:02X} offset {:08X} area_len {:04X}",
-                     rtype, var_len, offset, area_len);
+        match rtype {
+            0x20 => println!("DB  +${:04X}, ${:02X}", offset, area_len),
+            0x21 => println!("DW  +${:04X}, ${:02X}", offset, area_len),
+            0x23 => println!("DA  +${:04X}, ${:02X}", offset, area_len),
+            0x27 => println!("HEX +${:04X}, ${:02X}", offset, area_len),
+            0x28 => println!("DS  +${:04X}, ${:02X}", offset, area_len),
+            0x29 => println!("ASC +${:04X}, ${:02X}", offset, area_len),
+            _    => println!("rtype {:02X} var_len {:02X} offset {:08X} area_len {:04X}",
+                             rtype, var_len, offset, area_len)
         }
     }
     Ok(())
